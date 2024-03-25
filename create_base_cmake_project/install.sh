64dd09c89f1bfcb258e55487fb88a8201dc60450
peter@NeoGenesis <pieeaterpeter69@gmail.com>
2024-03-25 14:21:17 +0800

    edit install script

--- create_base_cmake_project/install.sh
+++ create_base_cmake_project/install.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/bash
 
-cp -v "create_cmake_project.sh" "${HOME}/.local/bin/";
+
+INSTALL_PATH="";
+if [ "$(id -u)" -eq 0 ]; then
+    INSTALL_PATH="/usr/local/bin/";
+
+else
+    INSTALL_PATH="${HOME}/.local/bin/";
+    mkdir -p "${INSTALL_PATH}";
+fi
+
+echo "install to ${INSTALL_PATH}";
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+
+cp -v "create_cmake_project.sh" "${INSTALL_PATH}";
