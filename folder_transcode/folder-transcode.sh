93fd7357f147d8fb771cb587201ae2276114bc99
peter <pieeaterpeter69@gmail.com>
2024-04-12 01:05:03 +0800

    set default profile: Rounding down by 1px when width is not divisible

--- folder_transcode/folder-transcode.sh
+++ folder_transcode/folder-transcode.sh
@@ -66,15 +66,15 @@ if [[ ! -d "${PROFILE_DIRNAME}" ]];
 then
     mkdir -p "${PROFILE_DIRNAME}";
 cat<<'EOF'>> "${PROFILE_DIRNAME}"/psp;
-FFMPEG_CONFIG='-vcodec libx264 -vf 'scale=480:-1' -crf 18 -profile:v main -level:v 2.1 -x264-params ref=3:bframes=1 -acodec aac -b:a 128k -ac 2 -movflags +faststart';
+FFMPEG_CONFIG='-vcodec libx264 -vf 'scale=480:-2' -crf 18 -profile:v main -level:v 2.1 -x264-params ref=3:bframes=1 -acodec aac -b:a 128k -ac 2 -movflags +faststart';
 VIDEO_SUFFIX="mp4"
 VIDEO_CONTAINER="mp4"
 EOF
 cat<<'EOF'>> "${PROFILE_DIRNAME}"/mipad1-1080;
-FFMPEG_CONFIG='-c:v h264 -vf 'scale=-1:1080' -crf 18';
+FFMPEG_CONFIG='-c:v h264 -vf 'scale=-2:1080' -crf 18';
 EOF
 cat<<'EOF'>> "${PROFILE_DIRNAME}"/mipad1-720;
-FFMPEG_CONFIG='-c:v h264 -vf 'scale=-1:720' -crf 18';
+FFMPEG_CONFIG='-c:v h264 -vf 'scale=-2:720' -crf 18';
 EOF
 fi
 
