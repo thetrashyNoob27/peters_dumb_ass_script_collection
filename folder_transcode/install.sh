0e08296734fec9f383395c6cfbad49c215f4766f
peter@NeoGenesis <pieeaterpeter69@gmail.com>
2024-04-12 02:12:19 +0800

    fix -vf issue

--- folder_transcode/install.sh
+++ folder_transcode/install.sh
@@ -4,5 +4,5 @@
 mkdir -pv "${HOME}/.local/bin/";
 rm "${HOME}/.local/bin/folder-transcode.sh";
 
-ln folder-transcode.sh "${HOME}/.local/bin/folder-transcode.sh";
+cp folder-transcode.sh "${HOME}/.local/bin/folder-transcode.sh";
 
